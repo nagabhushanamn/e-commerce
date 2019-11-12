a6a6b410432e86bcb0f9d3982c385a4bf1f9af5f
Nagabhushanam N <nag@Nagabhushanams-MacBook-Pro.local>
2019-11-12 15:16:45 +0400

    items api

--- items-api/routes/items.js
+++ items-api/routes/items.js
@@ -1,34 +1,82 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/eat_it_db', { useNewUrlParser: true });
 
-//----------------------------------------------------------
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+var itemSchema = new mongoose.Schema({
+    "name": String,
+    "price": Number,
+    "currency": String,
+    "qty": Number,
+    "isAvailable": Boolean,
+    "images": Array,
+    "ingredients": Object,
+    "hotel": Object,
+    "description": String,
+    "tags": Array
+});
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
+var Item = mongoose.model('Item', itemSchema, "items");  // Model
 
-// Database Name
-const dbName = 'eat_it_db';
 
-// Create a new MongoClient
-const client = new MongoClient(url);
 
-//----------------------------------------------------------
-// API - endpoint
+var reviewSchema = new mongoose.Schema({
+    "stars": Number,
+    "body": String,
+    "author": String,
+    "item": mongoose.Types.ObjectId,
+});
 
-router.get('/', function (req, res, next) {
-    client.connect(function (err, client) {
-        assert.equal(null, err);
-        const db = client.db(dbName);
-        // Get the collection
-        const col = db.collection('items');
-        col.find({}).toArray((err, result) => {
+var Review = mongoose.model('Review', reviewSchema, "reviews");  // Model
+
+
+
+router
+    .get('/', function (req, res, next) {
+        Item.find({}, (err, result) => {
+            if (err) throw err;
+            return res.json(result)
+        })
+    })
+    .post('/', function (req, res, next) {
+        let body = req.body;
+        let newItem = new Item(body);
+        newItem.save((err, result) => {
             if (err) throw err;
             res.json(result)
         })
-    });
-});
+    })
+    .put('/:itemId', function (req, res, next) {
+        let body = req.body;
+        let itemId = req.params.itemId;
+        Item.updateOne({ _id: mongoose.Types.ObjectId(itemId) }, body, (err, result) => {
+            if (err) throw err;
+            res.json(result)
+        })
+    })
+    .delete('/:itemId', function (req, res, next) {
+        Item.deleteOne({ _id: mongoose.Types.ObjectId(req.params.itemId) }, (err, result) => {
+            if (err) throw err;
+            res.json(result);
+        })
+    })
+    .post("/:itemId/reviews", (req, res, next) => {
+        let itemId = req.params.itemId;
+        let body = req.body;
+        let newReview = new Review(body);
+        newReview.item = mongoose.Types.ObjectId(itemId);
+        newReview.save((err, result) => {
+            if (err) throw err;
+            res.json(result)
+        })
+    })
+    .get("/:itemId/reviews", (req, res, next) => {
+        let itemId = req.params.itemId;
+        Review.find({ item: mongoose.Types.ObjectId(itemId) }, (err, result) => {
+            if (err) throw err;
+            res.json(result)
+        })
+    })
 
 module.exports = router;
